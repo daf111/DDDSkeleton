c18871685bcaebd757a8cd60ac1eb3a06b5d7b8d
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2021-11-01 15:06:44 -0300

    Implement Unit Testing with xUnit

---
+++ test/apps/VirtualWallet/Backend/Controllers/CashInPostControllerShould.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.test.Apps.VirtualWallet.Backend.Controllers
+{
+    public class CashInPostControllerShould
+    {
+    }
+}
---
+++ test/src/VirtualWallet/CashesIn/Application/Create/CashInCreatorShould.cs
@@ -0,0 +1,55 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.src.VirtualWallet.CashesIn.Domain;
+using Company.src.VirtualWallet.CashesIn.Application.Create;
+using Xunit;
+using Company.src.Shared.Domain.ValueObject;
+using Company.src.VirtualWallet.CashesIn.Domain.Exception;
+
+namespace Company.test.src.VirtualWallet.CashesIn.Application.Create
+{
+    public class CashInCreatorShould
+    {
+        protected readonly Mock<ICashInRepository> _repository;
+        protected readonly ICashInCreator _service;
+
+        public CashInCreatorShould()
+        {
+            _repository = new Mock<ICashInRepository>();
+            _service = new CashInCreator(_repository.Object);
+        }
+
+        [Fact]
+        public void it_should_create_a_non_existing_cashin()
+        {
+            CashIn cashIn = new CashIn(Guid.NewGuid(), "Test");
+            ShouldSearch(cashIn.Id, null);
+
+            _service.Create(cashIn.Id.Value, cashIn.Name.Value);            
+            ShouldCreate(cashIn);
+        }
+
+        [Fact]
+        public void it_should_not_create_an_existing_cashin()
+        {
+            CashIn cashIn = new CashIn(Guid.NewGuid(), "Test");
+            ShouldSearch(cashIn.Id, cashIn);
+
+            Assert.Throws<CashInExistsException>(() => _service.Create(cashIn.Id.Value, cashIn.Name.Value));            
+        }
+
+        private void ShouldSearch(IdentityValueObject id, CashIn? cashIn)
+        {
+            _repository.Setup(x => x.Find(id)).Returns(cashIn);
+        }
+        
+        private void ShouldCreate(CashIn cashIn)
+        {
+            _repository.Verify(x => x.Create(cashIn), Times.Once());
+        }
+    }
+}
